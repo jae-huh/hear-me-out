cd899ade6f1da944da7c1ba18d486ff655141299
Jae Huh <jaeeunhuh@gmail.com>
2017-05-18 22:45:39 +1200

    Make buttons in Buttons component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Button from './Button'
+import Buttons from './Buttons'
 import Display from './Display'
 
 class App extends React.Component {
@@ -10,27 +10,11 @@ class App extends React.Component {
       message: "Choose a category"
     }
     this.showMessage = this.showMessage.bind(this)
-    this.generateButtons = this.generateButtons.bind(this)
   }
 
   showMessage (msg) {
     this.setState({
       message: msg
-    })
-  }
-
-  generateButtons() {
-    const categories = [
-      {msg: "Small Talk"},
-      {msg: "Question"},
-      {msg: "Food"},
-      {msg: "Pain"},
-      {msg: "Need"},
-      {msg: "Place"},
-      {msg: "Person"}
-    ]
-    return categories.map((item) => {
-      return (<Button name={item.msg} showMessage={this.showMessage}/>)
     })
   }
 
@@ -41,9 +25,7 @@ class App extends React.Component {
           <h1>Hear Me Out</h1>
         </div>
         <Display message={this.state.message}/>
-        <div className="button-container">
-          {this.generateButtons()}
-        </div>
+        <Buttons showMessage={this.showMessage}/>
       </div>
     )
   }
--- src/Button.js
+++ src/Button.js
@@ -1,28 +1,29 @@
 import React from 'react'
-import './Button.css'
+// import './Button.css'
 import dinner from '../public/images/dinner.png'
 // import questionMark from '../public/images/question-mark.png'
 
 const Button = (props) => {
   const synth = window.speechSynthesis
-  const utter = new SpeechSynthesisUtterance(props.name)
+  const utter = new SpeechSynthesisUtterance(props.msg)
   function speak () {
     synth.speak(utter)
   }
 
   function speakShowMessage() {
     speak()
-    props.showMessage(props.name)
+    props.showMessage(props.msg)
   }
-
-  // Move generate button function from App and button container div + below
 
   return (
     <div className="button" onClick={speakShowMessage}>
-      <img className="btn-image" src={dinner} alt={props.name} />
-      <p className="keyword">{props.name}</p>
+      <img className="btn-image" src={props.img} alt={props.msg} />
+      <p className="keyword">{props.msg}</p>
     </div>
   )
 }
 
 export default Button
+
+/* <img className="btn-image" src={dinner} alt={props.name} />
+<p className="keyword">{props.name}</p> */
