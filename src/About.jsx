d779f11471e5283bc9d4aff9ebbd64725309a7fb
Jae Huh <jaeeunhuh@gmail.com>
2017-08-15 00:17:03 +1000

    Update about page

--- src/About.jsx
+++ src/About.jsx
@@ -11,7 +11,10 @@ const About = (props) => {
       <div className="additional-info-container">
         <div className="about-app">
           <p>
-            Hear Me Out was made for patients who are experiencing difficulties in communicating.
+            Hear Me Out was created to help patients in hospital, who are experiencing difficulties communicating due to their conditions.
+          </p>
+          <p>
+            It offers a default communication board that has basic patient needs with an ability to add customised boards the users can create. When a button that includes an icon and a corresponding word is pressed, the user can hear the word.
           </p>
           <p>
             It is an open-source project, and can be found at <a href="https://github.com/Jae-Huh/hear-me-out" target="blank">https://github.com/Jae-Huh/hear-me-out</a>
